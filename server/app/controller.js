3ab51fc3ece974ccbf49888484afb6a7c4b9b0dd
Seraphim Dmitrieff <dmitries1@hawkmail.newpaltz.edu>
2018-10-18 13:32:37 -0400

    update

--- server/app/controller.js
+++ server/app/controller.js
@@ -4,3 +4,7 @@ const {Interface, User} = require('./model');
 var interface = new Interface();
 
 const app = express.Router();
+
+app.get("/", function(req, res){
+    res.send({...interface});
+});
---
+++ server/app/model.js
@@ -0,0 +1,49 @@
+
+class Interface{
+
+    constructor() {
+        this.users = []
+    }
+
+    addUser(user) {
+       return this.users.push(user);
+    }
+
+    getUser(id){
+        for(var i=0; i<this.users.length; i++){
+            if(this.users[i].id == id){
+                return this.users[i];
+            }
+        }
+    }
+
+    deleteUser(id){
+
+    }
+}
+
+class User{
+    constructor(email, id){
+        this.url = email;
+        this.id = id;
+        this.profile = {
+            f_name: "",
+            l_name: "",
+            age: "",
+            email: this.url,
+        }
+        this.badges = [];
+        this.friends = [];
+    }
+
+    updateProfile({f_name, l_name, age}){
+        this.profile.f_name = f_name;
+        this.profile.l_name = l_name;
+        this.profile.age = age;
+    }
+
+}
+
+module.exports = {
+    Interface, User
+}
