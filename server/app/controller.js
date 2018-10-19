42279b9b144a07e88d7660bf47c4a73d9f34e5cd
Seraphim Dmitrieff <dmitries1@hawkmail.newpaltz.edu>
2018-10-18 22:02:18 -0400

    update for midterm

--- server/app/controller.js
+++ server/app/controller.js
@@ -8,3 +8,12 @@ const app = express.Router();
 app.get("/", function(req, res){
     res.send({...interface});
 });
+
+app.post('/users', (req, res) => {
+    const newUser = new User(req.body.url, app.users.length+1);
+    game.users.push(newUser);
+    res.send(newUser);
+});
+
+
+module.exports = app;
--- server/app/model.js
+++ server/app/model.js
@@ -17,9 +17,6 @@ class Interface{
         }
     }
 
-    deleteUser(id){
-
-    }
 }
 
 class User{
