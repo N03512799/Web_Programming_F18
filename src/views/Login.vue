845d525934ae83a4ff37d339dde68151687fcba1
Seraphim Dmitrieff <dmitries1@hawkmail.newpaltz.edu>
2018-12-01 21:58:01 -0500

    Added Forms

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -6,28 +6,27 @@
             Exercise App
           </div>
       </div>
-      <form class="text-right">
-  <div class="form-group row justify-content-center">
-    <div class="col-md-6 ">
-      <input type="email" class="form-control" id="inputEmail" placeholder="Email">
+  <form class="text-right">
+    <div class="form-group row justify-content-center">
+      <div class="col-md-6 ">
+        <input type="email" class="form-control" id="inputEmail" name="email" placeholder="Email">
+      </div>
     </div>
-  </div>
-  <div class="form-group row justify-content-center">
-    <div class="col-md-6">
-      <input type="password" class="form-control" id="inputPassword" placeholder="Password">
+    <div class="form-group row justify-content-center">
+      <div class="col-md-6">
+        <input type="password" class="form-control" id="inputPassword" name="password" placeholder="Password">
+      </div>
     </div>
-  </div>
-  <div class="form-group row justify-content-center">
-    <div class="col-xs-1 px-2">
-      <button type="submit" class="btn btn-primary">Sign in</button>
+    <div class="form-group row justify-content-center">
+      <div class="col-xs-1 px-2">
+        <button type="submit" class="btn btn-primary">
+          Sign in
+        </button>
+      </div>
+      <div class="col-xs-1 px-2">
+        <button type="button" class="btn btn-success" onclick="window.location.href='/newUser'">New User? Register Now</button>
+      </div>
     </div>
-    <div class="col-xs-1 px-2">
-      <button type="button" class="btn btn-primary" onclick="window.location.href='/newUser'">Register</button>
-    </div>
-  </div>
 </form>  
     </div>
 </template>
-<style>
-
-</style>
--- src/views/NewUser.vue
+++ src/views/NewUser.vue
@@ -1,58 +1,69 @@
 <template>
 <div>  
-  <div class="jumbotron text-center bg-info text-white">
+  <div class="jumbotron jumbotron-fluid text-center bg-info text-white">
     <div class="display-3">
       <img alt="Vue logo" src="@/assets/logo.png" id="logo">
-      Welcome
+      Welcome to Exercise App
+    </div>
+    <div>
+      <h3 class="mt-3">
+        Let us know a little about yourself
+      </h3>
     </div>
   </div>
-<div class="container">
-<form>
+<div class="container mt-5">
+<hr class="mx-5">
+<form action="/profile" method="get" class="mt-5">
   <div class="form-row">
-    <div class="form-group col-md-6">
-      <label for="inputEmail4">Email</label>
-      <input type="email" class="form-control" id="inputEmail4" placeholder="Email">
+    <div class="form-group col-md-6 justify-content-start">
+      <div class="text-danger">
+        <label for="f_name" class="col-md-3 mx-2">First Name*</label>
+        <input type="text" class="col-md-8 mx-2" id="f_name" name="f_name" placeholder="First Name (Required)">
+      </div>
     </div>
-    <div class="form-group col-md-6">
-      <label for="inputPassword4">Password</label>
-      <input type="password" class="form-control" id="inputPassword4" placeholder="Password">
-    </div>
-  </div>
-  <div class="form-group">
-    <label for="inputAddress">Address</label>
-    <input type="text" class="form-control" id="inputAddress" placeholder="1234 Main St">
-  </div>
-  <div class="form-group">
-    <label for="inputAddress2">Address 2</label>
-    <input type="text" class="form-control" id="inputAddress2" placeholder="Apartment, studio, or floor">
-  </div>
-  <div class="form-row">
-    <div class="form-group col-md-6">
-      <label for="inputCity">City</label>
-      <input type="text" class="form-control" id="inputCity">
-    </div>
-    <div class="form-group col-md-4">
-      <label for="inputState">State</label>
-      <select id="inputState" class="form-control">
-        <option selected>Choose...</option>
-        <option>...</option>
-      </select>
-    </div>
-    <div class="form-group col-md-2">
-      <label for="inputZip">Zip</label>
-      <input type="text" class="form-control" id="inputZip">
+    <div class="form-group col-md-6 justify-content">
+      <div class="text-danger">
+        <label for="l_name" class="col-md-3 mx-2">Last Name*</label>
+        <input type="text" class="col-md-8 mx-2" id="l_name" name="l_name" placeholder="Last Name (Required)">
+      </div>
     </div>
   </div>
-  <div class="form-group">
-    <div class="form-check">
-      <input class="form-check-input" type="checkbox" id="gridCheck">
-      <label class="form-check-label" for="gridCheck">
-        Check me out
-      </label>
+    <div class="form-row">
+    <div class="form-group col-md-6 justify-content-start">
+      <div class="text-danger">
+        <label for="email" class="col-md-3 mx-2">Email*</label>
+        <input type="email" class="col-md-8 mx-2" id="email" placeholder="Email (Required)">
+      </div>
+    </div>
+    <div class="form-group col-md-6 justify-content">
+      <div class="text-danger">
+        <label for="password" class="col-md-3 mx-2">Password*</label>
+        <input type="password" class="col-md-8 mx-2" id="password" name="password" placeholder="Password (Required)">
+      </div>
     </div>
   </div>
   <div class="form-group row justify-content-center">
-      <button type="submit" class="btn btn-primary">Create Profile</button>
+    <label for="gender" class="inline text-danger mx-2">Gender*: </label>
+    <div class="form-check-inline" id="gender">
+      <input class="form-check-input" type="radio" name="gender" id="male" value="option1" checked>
+      <label class="form-check-label" for="male">
+        Male
+      </label>
+    </div>
+    <div class="form-check-inline">
+      <input class="form-check-input" type="radio" name="gender" id="female" value="option2">
+      <label class="form-check-label" for="female">
+        Female
+      </label>
+    </div>
+  </div>
+  <hr class="mx-5">
+  <div class="text-center text-danger mb-4">
+    *Required Information
+  </div>
+  <div class="form-group row justify-content-center">
+      <button type="submit" class="btn btn-primary mx-2">Get Started</button>
+      <button type="button" class="btn btn-danger mx-2" onclick="window.location.href='/'">Go Back</button>
   </div>
 </form>
 </div>
