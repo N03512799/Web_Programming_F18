69c8b00aad2fd558a18486651bb6293004c6d89b
Seraphim Dmitrieff <dmitries1@hawkmail.newpaltz.edu>
2018-12-03 15:26:03 -0500

    updated profile

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -1,15 +1,16 @@
 
 <template>
 <div class= "container-fluid"> 
-	<div class = "row d-fluid inline">
-		<div class="col-lg-3 bg-dark order-2 order-lg-1 m-auto py-4 text-center ">
-			<h3 class="display-4 text-white">Workouts</h3>
+	<div class = "row d-fluid">
+		<div class="col-lg-3 bg-dark order-2 order-lg-1 p-4 text-center ">
+			<div class="my-4">
+      <h3 class="display-4 text-white">Workouts</h3>
 			<hr class="bg-light">
 			<div id="upper carousel" class="carousel slide py-3" data-ride="carousel">
   			<div class="carousel-inner">
     			<div class="carousel-item active pic">
-				    <img class="d-block w-100" src="@/assets/upper.jpg" alt="First slide">  						
-            <div class="overlay"></div>
+				    <img class="d-block w-100" src="@/assets/upper.jpg" alt="First slide">
+              <div class="overlay"></div>						
 					</div>
     			<div class="carousel-item pic">
       			<img class="d-block w-100" src="@/assets/upper1.jpg" alt="Second slide">	
@@ -70,6 +71,8 @@
   					</div>
 				</div>
 			</div>
+			<hr class="bg-light">
+      </div>
 			<div class="col-lg-6 text-center bg-info order-1 order-lg-2  ">
         <h1 class="display-2">Profile</h1>
 				<div class="container-fluid">
@@ -80,33 +83,64 @@
                 </div>
 					</div>
 				</div>
-				<div class="row">
+				<div class="row justify-content-center">
+          <div class=" card bg-info postsContainer my-4 ">
+            <div class="card-header bg-dark text-white w-100">  
+              <h3>Posts</h3>
+            </div>
+            <div class="card-body bg-light">
+              <div class="w-100 bg-light posts">
 
+              </div>
+              <div class="w-100 bg-light justify-content-center">
+                <button class="btn btn-primary my-3 w-50">Add Post</button>
+              </div>
+            </div>
+          </div>
 				</div>
 			</div>
-			<div class="col-lg-3 bg-success order-3 m-auto py-4 text-center">
-				<h3 class="display-4 text-white">Friends</h3>
-				<div class="card m-auto py-5">
+			<div class="col-lg-3 bg-success order-3 p-4 text-center">
+				<div class="my-4">
+        <h3 class="display-4 text-white">Friends</h3>
+        <hr class="bg-light">
+				<div class="card my-4 ">
+            <img class="card-img-top" src="" alt="Card image cap">
+            <div class="card-body">
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            </div>
+				</div>
+				<h3 class="display-4 text-white">Pictures</h3>
+        <hr class="bg-light">
+				<div class="card my-4">
             <img class="card-img-top" src="" alt="Card image cap">
             <div class="card-body">
               <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
             </div>
 				</div>
 			</div>
+      </div>
 		</div>
 </div>
 </template>
 <style>
 	.pic:hover > .overlay {
-			position: absolute;
-			top: 0;
-			width:100%;
-			height:100%;
-			left: 0;
-			background-color:#000;
-			opacity:0.5;
-			z-index: 100;
-			display: block;
+		  position: absolute;
+		  top: 0;
+		  width:100%;
+		  height:100%;
+		  left: 0;
+		  background-color:#FFF;
+		  opacity:0.3;
+		  z-index: 100;
+		  display: block;
 	}
-</style>
 
+  .posts {
+    height: 400px;
+    overflow-y: scroll;
+  }
+
+  .postsContainer{
+    width: 90%;
+  }
+</style>
